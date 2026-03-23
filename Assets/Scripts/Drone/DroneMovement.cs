5618b7a290472911a120cd9d74ee237859eae665
Elianfarias <dev.elian.farias@gmail.com>
2026-03-23 06:31:34 -0300

    add drone model

---
+++ Assets/Scripts/Drone/DroneMovement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DroneMovement : MonoBehaviour
+{
+    public InputSystem_Actions inputActions;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
